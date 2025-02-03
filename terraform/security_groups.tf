62a5ca077d4af4fd182c06e49acd2ebda2912454
Md.Tarequl Islam <tareq474@gmail.com>
2025-02-03 22:50:11 +0600

    Updated

---
+++ terraform/security_groups.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
